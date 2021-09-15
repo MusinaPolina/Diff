8119f89b56a1d6b45a04ba9cc91f94f3cefbaf43
MusinaPolina <pmusina03@gmail.com>
2021-09-15 20:52:17 +0300

    class Text contains the input in form of list of strings

---
+++ src/main/kotlin/Text.kt
@@ -0,0 +1,11 @@
+/*
+* class Text contains list of strings for comparison.
+* Strings supposed to be received from input.
+*/
+
+class Text {
+    val text: List<String> = listOf()
+    constructor() {
+        TODO("input parsing")
+    }
+}
